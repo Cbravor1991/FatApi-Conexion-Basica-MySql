7f0ed5b8b0ba4578a90c5f56bf6faa9699067a42
Cbravor1991 <cbravor@fi.uba.ar>
2024-04-23 03:59:21 -0300

    agrego schema de users y creo usuario en la base de datos

--- routes/user.py
+++ routes/user.py
@@ -1,9 +1,24 @@
 from fastapi import APIRouter
 from config.db import connection
 from models.user import users
+from schemas.user import User
+from cryptography.fernet import Fernet
+
+#inicializo fernet para encriptar la contraseña
+key = Fernet.generate_key()
+cripto = Fernet(key)
 
 user = APIRouter()
 
 @user.get("/users")
-def helloworld():
-    return "hello world"
+def get_users():
+    return connection.execute(users.select()).fetchall()
+
+@user.post("/users")
+def create_user(user: User):
+    new_user = {"name": user.name, "email": user.email}
+    new_user["password"] = cripto.encrypt(user.password.encode("utf-8"))
+    result = connection.execute(users.insert().values(new_user))
+    print(result.lastrowid)
+    return connection.execute(users.select().where(users.c.id == result.lastrowid)).first()
+
