4bea346c588d1e284958f0bb8d5605cea4d2899a
Cbravor1991 <cbravor@fi.uba.ar>
2024-04-24 00:16:53 -0300

    crud usuario basico actualizado

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from routes.user import user
 #ejecutar modulo fasta api
 '''uvicorn app:app --reload
 '''
-app = FastAPI()
+app = FastAPI(
+    title = "CRUD BASICO USUARIOS",
+    description= "Consultas basicas a la base de datos sobre tabla usuarios",
+    version = "1.0.0",
+    openapi_tags=[{
+        "name": "users",
+        "description": "users routes"
+    }]
+)
 
 app.include_router(user)
--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from config.db import connection
 from models.user import users
 from schemas.user import User
+from starlette.status import HTTP_204_NO_CONTENT
 from cryptography.fernet import Fernet
 
 #inicializo fernet para encriptar la contraseña
@@ -10,11 +11,11 @@ cripto = Fernet(key)
 
 user = APIRouter()
 
-@user.get("/users")
+@user.get("/users", response_model = list[User], tags = ["users"])
 def get_users():
     return connection.execute(users.select()).fetchall()
 
-@user.post("/users")
+@user.post("/users", response_model = User, tags = ["users"])
 def create_user(user: User):
     new_user = {"name": user.name, "email": user.email}
     new_user["password"] = cripto.encrypt(user.password.encode("utf-8"))
@@ -22,9 +23,24 @@ def create_user(user: User):
     print(result.lastrowid)
     return connection.execute(users.select().where(users.c.id == result.lastrowid)).first()
 
-@user.get("/user/{id}")
-def hello_world(id:str):
+@user.get("/user/{id}", response_model = User,tags = ["users"])
+def get_user(id:str):
     return connection.execute(users.select().where(users.c.id==id)).first()
+
+@user.delete("/user/{id}", status_code = status.HTTP_204_NO_CONTENT, tags = ["users"] )
+def delete_user(id:str, user: User):
+    connection.execute(users.update().values(name = user.name, 
+                                             email =user.email,
+                                             password= user.password).where(users.c.id == id))
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+@user.put("/user/{id}", response_model= User, tags = ["users"])
+def update_user(id:str, user: User):
+    connection.execute(users.update().values(name = user.name, 
+                                             email =user.email,
+                                             password= cripto.encrypt(user.password.encode("utf-8"))).where(users.c.id == id))
+    return connection.execute(users.select().where(users.c.id==id)).first()
+
 
 
 
